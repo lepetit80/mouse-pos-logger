45c1f5609f11a9631b78413612eed7a4967cb468
Terry Lee <lepetit80@gmail.com>
2017-11-02 12:40:18 +0900

    ng g enum log-level

--- src/app/my-special-logger.service.ts
+++ src/app/my-special-logger.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MySpecialLoggerService {
+  logLevel: LogLevel;
+  logs: string[] = {};
+  private readonly MAX_HISTORY_CNT: number = 100;
+  private readonly TIME_FORMATTER: string = "YYYY-MM-DD HH:mm:ss.SSS";
 
-  constructor() { }
+  constructor(logLevel: LogLevel) {
+    this.logLevel = logLevel;
+  }
 
 }
