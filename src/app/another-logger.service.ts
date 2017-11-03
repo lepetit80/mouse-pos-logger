b5b55e5169daeb70b00077fbac3249d35821b353
Terry Lee <lepetit80@gmail.com>
2017-11-03 11:04:45 +0900

    add AnotherLoggerService

--- src/app/another-logger.service.ts
+++ src/app/another-logger.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { LogLevel } from './log-level.enum';
+import * as format from 'date-fns/format';
+import { LOG_LEVEL_TOKEN } from './app.tokens';
+import { LoggerService } from './logger-service';
 
 @Injectable()
-export class AnotherLoggerService {
+export class AnotherLoggerService extends LoggerService {
 
-  constructor() { }
+  log(logLevel: LogLevel, msg: string) {
+    const logMsg = this.getLogMsg(logLevel, msg);
+    if (this.isProperLogLevel(logLevel)) {
+      console.log(logMsg);
+    }
+  }
+
+  constructor(@Inject(LOG_LEVEL_TOKEN) logLevel: LogLevel) {
+    super(logLevel);
+  }
+
+  private getLogMsg(logLevel: LogLevel, msg: string) {
+    return `[${(LogLevel[logLevel])}] - ${msg}`;
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MouseTrackZoneComponent } from './mouse-track-zone/mouse-track-zone.component';
 import { MySpecialLoggerService } from './my-special-logger.service';
+import { AnotherLoggerService } from './another-logger.service';
 import { LogLevel } from './log-level.enum';
 import { LOG_LEVEL_TOKEN } from './app.tokens';
 
@@ -15,7 +16,11 @@ import { LOG_LEVEL_TOKEN } from './app.tokens';
   imports: [
     BrowserModule
   ],
-  providers: [MySpecialLoggerService, { provide: LOG_LEVEL_TOKEN, useValue: LogLevel.DEBUG}],
+  providers: [
+    MySpecialLoggerService,
+    AnotherLoggerService,
+    { provide: LOG_LEVEL_TOKEN, useValue: LogLevel.DEBUG}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
