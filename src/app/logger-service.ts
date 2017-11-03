490f2b6a58e82738698c940b9c433f0ab087e0bf
Terry Lee <lepetit80@gmail.com>
2017-11-03 10:56:46 +0900

    add LoggerService

--- src/app/logger-service.ts
+++ src/app/logger-service.ts
@@ -1,2 +1,24 @@
-export class LoggerService {
+import { LogLevel } from './log-level.enum';
+
+export abstract class LoggerService {
+    logLevel: LogLevel;
+
+    constructor(logLevel: LogLevel) {
+        this.logLevel = logLevel;
+    }
+
+    trace(msg: string) { this.log(LogLevel.TRACE, msg); }
+    debug(msg: string) { this.log(LogLevel.DEBUG, msg); }
+    info(msg: string) { this.log(LogLevel.INFO, msg); }
+    warn(msg: string) { this.log(LogLevel.WARN, msg); }
+    error(msg: string) { this.log(LogLevel.ERROR, msg); }
+    abstract log(logLevel: LogLevel, msg: string);
+
+    protected isProperLogLevel(logLevel: LogLevel): boolean {
+        if (this.logLevel === LogLevel.DEBUG) {
+            return true;
+        } else {
+            return logLevel >= this.logLevel;
+        }
+    }
 }
--- src/app/my-special-logger.service.ts
+++ src/app/my-special-logger.service.ts
@@ -2,27 +2,29 @@ import { Injectable, Inject } from '@angular/core';
 import { LogLevel } from './log-level.enum';
 import * as format from 'date-fns/format';
 import { LOG_LEVEL_TOKEN } from './app.tokens';
+import { LoggerService } from './logger-service';
 
 @Injectable()
-export class MySpecialLoggerService {
+export class MySpecialLoggerService extends LoggerService {
   logLevel: LogLevel;
   logs: string[] = [];
   private readonly MAX_HISTORY_CNT: number = 100;
   private readonly TIME_FORMATTER: string = 'YYYY-MM-DD HH:mm:ss.SSS';
 
   constructor( @Inject(LOG_LEVEL_TOKEN) logLevel: LogLevel) {
-    this.logLevel = logLevel;
+    // this.logLevel = logLevel;
+    super(logLevel);
   }
 
   // constructor(logLevel: LogLevel) {
   //   this.logLevel = logLevel;
   // }
 
-  trace(msg: string) { this.log(LogLevel.TRACE, msg); }
-  debug(msg: string) { this.log(LogLevel.DEBUG, msg); }
-  info(msg: string) { this.log(LogLevel.INFO, msg); }
-  warn(msg: string) { this.log(LogLevel.WARN, msg); }
-  error(msg: string) { this.log(LogLevel.ERROR, msg); }
+  // trace(msg: string) { this.log(LogLevel.TRACE, msg); }
+  // debug(msg: string) { this.log(LogLevel.DEBUG, msg); }
+  // info(msg: string) { this.log(LogLevel.INFO, msg); }
+  // warn(msg: string) { this.log(LogLevel.WARN, msg); }
+  // error(msg: string) { this.log(LogLevel.ERROR, msg); }
 
   log(logLevel: LogLevel, msg: string) {
     const logMsg = this.getFormattedLogMsg(logLevel, msg);
@@ -39,13 +41,13 @@ export class MySpecialLoggerService {
     this.logs.push(logMsg);
   }
 
-  private isProperLogLevel(logLevel: LogLevel): boolean {
-    if (this.logLevel === LogLevel.TRACE) {
-      return true;
-    } else {
-      return logLevel >= this.logLevel;
-    }
-  }
+  // private isProperLogLevel(logLevel: LogLevel): boolean {
+  //   if (this.logLevel === LogLevel.TRACE) {
+  //     return true;
+  //   } else {
+  //     return logLevel >= this.logLevel;
+  //   }
+  // }
 
   private getFormattedLogMsg(logLevel: LogLevel, msg: string) {
     const curTimestamp = format(new Date(), this.TIME_FORMATTER);
