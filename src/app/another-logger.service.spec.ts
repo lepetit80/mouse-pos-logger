e4ce7ba7161e058c3c8b9a02dd2dab8a8ff846dd
Terry Lee <lepetit80@gmail.com>
2017-11-03 12:59:15 +0900

    service test

--- src/app/another-logger.service.spec.ts
+++ src/app/another-logger.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AnotherLoggerService } from './another-logger.service';
+import { LogLevel } from './log-level.enum';
+import { LOG_LEVEL_TOKEN } from './app.tokens';
 
 describe('AnotherLoggerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AnotherLoggerService]
+      providers: [AnotherLoggerService, { provide: LOG_LEVEL_TOKEN, useValue: LogLevel.DEBUG }]
     });
   });
 
   it('should be created', inject([AnotherLoggerService], (service: AnotherLoggerService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('최초의 로그 레벨은 LOG_LEVEL_TOKEN에서 선언한 값이어야 한다.',
+    inject([AnotherLoggerService], (service: AnotherLoggerService) => {
+      expect(service).toBeTruthy();
+      expect(service.logLevel).toEqual(LogLevel.DEBUG);
   }));
 });
--- src/app/my-special-logger.service.spec.ts
+++ src/app/my-special-logger.service.spec.ts
@@ -1,15 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MySpecialLoggerService } from './my-special-logger.service';
+import { LogLevel } from './log-level.enum';
+import { LOG_LEVEL_TOKEN } from './app.tokens';
 
-describe('MySpecialLoggerService', () => {
+fdescribe('MySpecialLoggerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MySpecialLoggerService]
+      providers: [MySpecialLoggerService, { provide: LOG_LEVEL_TOKEN, useValue: LogLevel.DEBUG }]
     });
   });
 
   it('should be created', inject([MySpecialLoggerService], (service: MySpecialLoggerService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('로그 레벨애 따라 저장되는 로그의 수가 정확하게 일치해야 한다.',
+    inject([MySpecialLoggerService], (service: MySpecialLoggerService) => {
+      service.debug('test 1');
+      service.debug('test 2');
+      // expect(service).toBeTruthy();
+      expect(service.logs.length).toEqual(2);
   }));
 });
