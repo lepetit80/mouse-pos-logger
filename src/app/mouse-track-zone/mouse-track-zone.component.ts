9f497474118bb6d89a2105b065d2a3f63727b71c
Terry Lee <lepetit80@gmail.com>
2017-11-13 16:56:12 +0900

    using RxJs

--- src/app/mouse-track-zone/mouse-track-zone.component.ts
+++ src/app/mouse-track-zone/mouse-track-zone.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, Input, Host, Optional} from '@angular/core';
+import { Component, OnInit, Input, Host, Optional, ViewEncapsulation} from '@angular/core';
 import { LoggerService } from '../logger-service';
 import { MySpecialLoggerService } from '../my-special-logger.service';
 import { AnotherLoggerService } from '../another-logger.service';
 import { LogLevel } from '../log-level.enum';
+import { Observable } from 'rxJs/Observable';
+import { Subject } from 'rxJs/Subject';
 
 @Component({
   selector: 'mpl-mouse-track-zone',
   templateUrl: './mouse-track-zone.component.html',
-  styleUrls: ['./mouse-track-zone.component.css']
+  styleUrls: ['./mouse-track-zone.component.css'],
+  // providers: [],
+  encapsulation: ViewEncapsulation.Native
 })
 export class MouseTrackZoneComponent implements OnInit {
   // @Input() private logger: MySpecialLoggerService;
@@ -22,6 +26,8 @@ export class MouseTrackZoneComponent implements OnInit {
   // }
 
   logger: LoggerService;
+  moveSubject: Subject<MouseEvent> = new Subject<MouseEvent>();
+  move$: Observable<MouseEvent> = this.moveSubject.asObservable();
 
   constructor(
     @Host() @Optional() mySpecialLogger: MySpecialLoggerService,
@@ -31,6 +37,10 @@ export class MouseTrackZoneComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.move$
+      .throttleTime(1000)
+      .map(evt => [evt.clientX, evt.clientY])
+      .subscribe(pos => this.logger.info(`x:${pos[0]} y:${pos[1]}`));
   }
 
   captureMousePos($event: MouseEvent) {
